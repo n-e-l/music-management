abe5c7da10451e8a635ca284d1081a0c6e67649f
Lauda Carly <lauda@nel.re>
2026-01-05 10:51:25 +0100

    Fix flake

--- flake.nix
+++ flake.nix
@@ -30,11 +30,15 @@
             cargo-edit
 
             openssl
+            pkg-config
           ];
 
           # Environment variables
           RUST_SRC_PATH = "${rustToolchain}/lib/rustlib/src/rust/library";
           PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
+          OPENSSL_DIR = "${pkgs.openssl.dev}";
+          OPENSSL_LIB_DIR = "${pkgs.openssl.out}/lib";
+          OPENSSL_INCLUDE_DIR = "${pkgs.openssl.dev}/include";
         };
       });
 }
